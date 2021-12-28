35fc676bb55a6aed2d2f1a55e48415b1df246412
Benoit Galati <benoit.galati@gmail.com>
2021-12-28 08:26:23 +0100

    TDD By Example: Money - Chapter 7

--- tdd-by-example-book/src/Money/Money.php
+++ tdd-by-example-book/src/Money/Money.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace App\Money;
 
+use JetBrains\PhpStorm\Immutable;
+use JetBrains\PhpStorm\Pure;
+
+#[Immutable]
 abstract class Money
 {
     protected int $amount;
@@ -13,13 +17,16 @@ abstract class Money
         $this->amount = $amount;
     }
 
-    public function times(int $times): self
+    #[Pure]
+    public function times(int $times): static
     {
         return new static($this->amount * $times);
     }
 
     public function equals(Money $money): bool
     {
-        return $this->amount === $money->amount;
+        return
+            get_class($money) === get_class($this)
+            && $this->amount === $money->amount;
     }
 }
