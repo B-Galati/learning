110081e7f60d875806b41d654247550347c92ec6
Benoit Galati <benoit.galati@gmail.com>
2022-01-05 08:59:37 +0100

    TDD By Example: Money - Chapter 15

--- tdd-by-example-book/src/Money/Sum.php
+++ tdd-by-example-book/src/Money/Sum.php
@@ -11,6 +11,10 @@ final class Sum implements Expression
 
     public function reduce(Bank $bank, string $to): Money
     {
-        return new Money($this->augend->amount + $this->addend->amount, $to);
+        return new Money(
+            $this->augend->reduce($bank, $to)->amount
+            + $this->addend->reduce($bank, $to)->amount,
+            $to
+        );
     }
 }
