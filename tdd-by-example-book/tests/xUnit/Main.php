856cd1160a1cc0ed84591a0c7de2bdeb6ce56f41
Benoit Galati <benoit.galati@gmail.com>
2022-01-08 14:59:43 +0100

    TDD By Example Chapter 18

--- tdd-by-example-book/tests/xUnit/Main.php
+++ tdd-by-example-book/tests/xUnit/Main.php
@@ -1,13 +1,22 @@
-<?php
+<?php /** @noinspection PhpIllegalPsrClassPathInspection */
 
 declare(strict_types=1);
 
 require './../../vendor/autoload.php';
 
+use App\xUnit\TestCase;
 use App\xUnit\WasRun;
 
-$test = new WasRun("testMethod");
-echo $test->wasRun;
-echo "\n";
-$test->run();
-echo $test->wasRun;
+class TestCaseTest extends TestCase
+{
+    public function testRunning(): void
+    {
+        $test = new WasRun("testMethod");
+        echo $test->wasRun;
+        echo "\n";
+        $test->run();
+        echo $test->wasRun;
+    }
+}
+
+(new TestCaseTest('testRunning'))->run();
