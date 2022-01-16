7e02d8273ce818bf3b73d4bfbfc1635406ac6f77
Benoit Galati <benoit.galati@gmail.com>
2022-01-16 14:58:14 +0100

    bubble sort

--- katas-php/tests/SortTest.php
+++ katas-php/tests/SortTest.php
@@ -13,17 +13,24 @@ class SortTest extends TestCase
         $this->assertSame([1], bubbleSort([1]), '[1]');
         $this->assertSame([1, 2], bubbleSort([1, 2]), '[1, 2]');
         $this->assertSame([1, 2], bubbleSort([2, 1]), '[2, 1]');
+        $this->assertSame([1, 2, 3], bubbleSort([1, 2, 3]), '[1, 2, 3]');
+        $this->assertSame([1, 2, 3], bubbleSort([2, 1, 3]), '[2, 1, 3]');
+        $this->assertSame([1, 2, 3], bubbleSort([1, 3, 2]), '[1, 3, 2]');
+        $this->assertSame([1, 2, 3], bubbleSort([3, 2, 1]), '[3, 2, 1]');
     }
 }
 
 function bubbleSort(array $list): array
 {
     if (count($list) > 1) {
-        $first = $list[0];
-        $second = $list[1];
+        for ($i = 0; $i < count($list) - 1; $i++) {
+            $first  = $list[$i];
+            $second = $list[$i + 1];
 
-        if ($first > $second) {
-            return [$second, $first];
+            if ($first > $second) {
+                $list[$i] = $second;
+                $list[$i + 1] = $first;
+            }
         }
     }
 
